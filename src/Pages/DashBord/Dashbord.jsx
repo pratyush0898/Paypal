6739fa32b9760ee344b9aa64e590bedbe4bf4454
Pratyush0898 <priyankarangwa@gmail.com>
2024-08-18 16:00:59 +0530

    Fix bug

--- src/Pages/DashBord/Dashbord.jsx
+++ src/Pages/DashBord/Dashbord.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './DashBord.css'
 import NavBar from '../../Components/NavBar/NavBar'
-import Transition from '../../Components/Transition/transition'
+import Transition from '../../Components/Transition/Transition'
 import ScanPay from '../../Components/SacnPay/ScanPay'
 import Bills from '../../Components/Bills/Bills'
 import Invite from '../../Components/Invite/Invite'
