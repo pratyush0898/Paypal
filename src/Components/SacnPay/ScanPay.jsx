71fe2045e865a805258cebd6e6adfcbdb4f35323
Pratyush0898 <priyankarangwa@gmail.com>
2024-08-21 17:16:33 +0530

    Creat ScanPay

--- src/Components/SacnPay/ScanPay.jsx
+++ src/Components/SacnPay/ScanPay.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import './ScanPay.css'
+import React from "react";
+import ScanPayIMG from "/ScanPay.png";
+import "./ScanPay.css";
 
 const ScanPay = () => {
   return (
-    <>
-      {/* Contents */}
-    </>
-  )
-}
+    <div className="ScanPay">
+      <div className="img">
+        <img src={ScanPayIMG} alt="" />
+      </div>
+      <div className="content">
+        <div className="head">
+          <h1>Scan To Pay</h1>
+        </div>
+        <br />
+        <div className="para">
+          <p>
+            Effortlessly complete transactions by scanning secure QR codes,
+            ensuring fast and reliable payments.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ScanPay
+export default ScanPay;
--- src/Components/Transition/Transition.jsx
+++ src/Components/Transition/Transition.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import './Transition.css'
+import React from "react";
+import Bill from "/Bill.svg";
+import "./Transition.css";
 
 const Transition = () => {
   return (
-    <>
-      {/* Contents */}
-    </>
-  )
-}
+    <div className="transition-wapper">
+      <div className="transition">
+        <div className="text">
+          <h1>Making transitions easy</h1>
+        </div>
+        <div className="shortcut-wapper">
+          <div className="row-1">
+            <div className="shortcut">
+              <img src={Bill} />
+              <span>Scan QR code</span>
+            </div>
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Pay Contact</span>
+            </div>  
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Pay Phone number</span>
+            </div>
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Bank transfer</span>
+            </div>
+          </div>
+          <div className="row-2">
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Payments</span>
+            </div>
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Pay UPI id</span>
+            </div>
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Self transfer</span>
+            </div>
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Pay bills</span>
+            </div>
+            
+            <div className="shortcut">
+              <img src={Bill}/>
+              <span>Mobile recharge</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Transition
+export default Transition;
